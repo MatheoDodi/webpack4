9e672c0ba3800333ac5033ed8f78e8786a7a258a
Matthew Dodi <matthew.dodi@gmail.com>
2019-03-03 22:53:01 -0800

    Hot Reloading both Client and Server with Nodemon

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -13,7 +14,6 @@ module.exports = {
   },
   devServer: {
     contentBase: 'dist',
-    overlay: true,
     hot: true
   },
   module: {
@@ -42,15 +42,6 @@ module.exports = {
         test: /\.html$/,
         use: [
           {
-            loader: 'file-loader',
-            options: {
-              name: '[name].html'
-            }
-          },
-          {
-            loader: 'extract-loader'
-          },
-          {
             loader: 'html-loader',
             options: {
               attrs: ['img:src']
@@ -71,5 +62,10 @@ module.exports = {
       }
     ]
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 };
